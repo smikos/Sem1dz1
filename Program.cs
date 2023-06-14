5c982de3eff97c1cb4aa99f6707b3552d7ff6f85
Konstantin Smirnov <sasharugby@yandex.ru>
2023-06-14 20:49:15 +0300

    fin

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число 1 =>");
+string inputedString = Console.ReadLine(); 
+Console.Write("Введите число 2 =>"); 
+string inputedString2 = Console.ReadLine(); 
+int number1 = Convert.ToInt32(inputedString);
+int number2 = Convert.ToInt32(inputedString2);
+int max=number1;
+int min=number2;
+if (max>min)
+{
+    Console.WriteLine("Большее число " + number1);
+    Console.WriteLine("Меньшое число " + number2 );
+
+}
+else 
+Console.WriteLine("Большее число " + number2);
+    Console.WriteLine("Меньшое число " + number1 );
